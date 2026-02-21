d63543620d63868bd1b7e93a914e5b64eb2332be
thiago-d3v <thiago.dev812@gmail.com>
2026-02-21 02:15:41 -0300

    Add files via upload

---
+++ primeiro_e_ultimo_nome.py
@@ -0,0 +1,7 @@
+name = input('Digite seu nome completo: ')
+div = name.split()
+first = div[0]
+end = div[-1]
+print('Seu primeiro nome é {}'.format(first))
+print('Seu ultimo nome é {}'.format(end))
+
